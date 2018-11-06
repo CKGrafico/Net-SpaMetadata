f5b54bdf6bdbaa74db7132a0ea0c9cc3508c32a8
Quique Fdez Guerra <quique@ckgrafico.com>
2018-11-06 19:10:40 +0100

    Fix bug

--- SpaMetadata/ExampleSpaMetadata/Startup.cs
+++ SpaMetadata/ExampleSpaMetadata/Startup.cs
@@ -24,7 +24,7 @@ namespace ExampleSpaMetadata
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddScoped<IExampleDataService, ExampleDataService>()
+            services.AddScoped<IExampleDataService, ExampleDataService>();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
